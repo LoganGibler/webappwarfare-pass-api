962a57449b94692aeea5948daff1d769763ad470
LoganGibler <Logan.Gibler@gmail.com>
2023-08-14 23:03:58 -0400

    flask api for webappwarfare hash passwords function completed- just need to figure out how to deploy

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS, cross_origin
+from flask import request
+import string
+import random
+import json
+import re
+
+from hashlib import md5, sha1, sha224, sha256, sha384, sha512
+
+server = Flask(__name__)
+cors = CORS(server)
+server.config['CORS_HEADERS'] = 'Content-Type'
+
+
+@server.route("/hash_pass", methods=['GET', 'POST'])
+@cross_origin()
+def hash_pass():
+     data = request.get_json(force=True)
+     print("This is main request to sent to flask api:",data)
+     password = data['password']
+     print("This is password: "+ password)
+
+     hashed_password = md5(password.encode())
+
+     return {"hashed_pass": hashed_password.hexdigest()}
+   
+ 
+
+
+if __name__ == "__main__":
+    server.run(debug="True")
