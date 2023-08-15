d243106b84b0dd727c38a17e505925bba45c407a
LoganGibler <Logan.Gibler@gmail.com>
2023-08-14 23:27:11 -0400

    changing connection string

--- server.py
+++ server.py
@@ -16,17 +16,16 @@ server.config['CORS_HEADERS'] = 'Content-Type'
 @server.route("/hash_pass", methods=['GET', 'POST'])
 @cross_origin()
 def hash_pass():
-     data = request.get_json(force=True)
-     print("This is main request to sent to flask api:",data)
-     password = data['password']
-     print("This is password: "+ password)
+    data = request.get_json(force=True)
+    print("This is main request to sent to flask api:", data)
+    password = data['password']
+    print("This is password: " + password)
 
-     hashed_password = md5(password.encode())
+    hashed_password = md5(password.encode())
 
-     return {"hashed_pass": hashed_password.hexdigest()}
-   
- 
+    return {"hashed_pass": hashed_password.hexdigest()}
 
 
 if __name__ == "__main__":
-    server.run(debug="True")
+    server.run(host="0.0.0.0",
+               port=5000)
